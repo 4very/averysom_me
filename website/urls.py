e91f823171d5df6af26541449cb4013e27e6eb4c
sommea <asommer999@gmail.com>
2019-01-21 22:43:57 -0500

    Updated Django

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from website import views
-
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
     url('^/about$', views.AboutPageView.as_view()),
+    url(r'^admin/', admin.site.urls)
 ]
